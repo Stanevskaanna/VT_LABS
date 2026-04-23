a8e3e7dd1d4a61f4a129f6e94c17536772e6600c
Stanevskaya Anna <annastanevskaa668@gmail.com>
2026-04-23 14:08:25 +0300

    LABA 05 DONE

---
+++ LAB05/LAB05/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LAB05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void onPlusClick(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                label4.Text = "Введите число";
+                label4.ForeColor = Color.Red;
+            }
+            else
+            {
+                int Sum = int.Parse(textBox1.Text) + int.Parse(textBox2.Text);
+                label4.Text = Sum.ToString();
+            }
+        }
+
+        private void onMinusClick(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                label4.Text = "Введите число";
+                label4.ForeColor = Color.Red;
+            }
+            else
+            {
+                int Min = int.Parse(textBox1.Text) - int.Parse(textBox2.Text);
+                label4.Text = Min.ToString();
+            }
+        }
+        private void onMultiplyClick(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                label4.Text = "Введите число";
+                label4.ForeColor = Color.Red;
+            }
+            else
+            {
+                int Mul = int.Parse(textBox1.Text) * int.Parse(textBox2.Text);
+                label4.Text = Mul.ToString();
+            }
+        }
+
+        private void onDivideClick(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                label4.Text = "Введите число";
+                label4.ForeColor = Color.Red;
+            }
+
+
+            else if (int.Parse(textBox2.Text) == 0)
+            {
+                label4.Text = "Ошибка:делить на ноль нельзя";
+                label4.ForeColor = Color.Red;
+            }
+            else
+            {
+                int Div = int.Parse(textBox1.Text) / int.Parse(textBox2.Text);
+                label4.Text = Div.ToString();
+            }
+        }
+        private void onClearClick(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            label4.Text = "0";
+        }
+    }
+}
+
+    
+
+
+        
+
+
+
+
+
+
+
+
+    
---
+++ LAB05/LAB05/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("LAB05")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LAB05")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("608111e5-72b4-4f45-b908-c154e82bea09")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
